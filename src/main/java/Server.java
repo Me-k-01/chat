000922eba4453b3bcfba1870a410905b008328c9
Me-k-01 <mecaman01@gmail.com>
2021-11-25 11:12:09 +0100

    break while when bye

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -79,10 +79,11 @@ public class Server extends Thread {
         String usrInput = null;
         start(); // démarage du thread pour la reception    
 
-        while ((usrInput = stdIn.readLine() ) != null && ! usrInput.equals("bye") ) { // Tant que l'on a des input
+        while ((usrInput = stdIn.readLine() ) != null) { // Tant que l'on a des input
             byte[] encryptedText = aes.encryptText(usrInput);
             out.writeInt(encryptedText.length);
             out.write(encryptedText);
+            if (! usrInput.equals("bye")) { break; }
         } 
         this.isStopped = true;
         out.close();
