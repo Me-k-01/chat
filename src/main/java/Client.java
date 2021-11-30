427626171214377fbae4a455567686da1dc122fd
Me-k-01 <mecaman01@gmail.com>
2021-11-30 15:20:09 +0100

    conf

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.Properties;
 
 public class Client extends Server {
 
@@ -9,8 +10,19 @@ public class Client extends Server {
 
     @Override
     public void connect()  {
-        String conAddress = "192.168.58.75";
+        String conAddress = "192.168.4.75";
         int conPort = 4444;
+
+        Properties prop = new Properties();
+        String fileName = "config.conf";
+        try (FileInputStream fis = new FileInputStream(fileName)) {
+            prop.load(fis);
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+        conAddress = (String)prop.get("SERVER_ADDRESS");
+        conPort = (Integer)prop.get("SERVER_PORT");
+
         try{
             Socket echoSocket = new Socket(InetAddress.getByName(conAddress), conPort) ; 
             out = new DataOutputStream(echoSocket.getOutputStream());
